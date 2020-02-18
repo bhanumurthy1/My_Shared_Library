4cf6d46c70149ca6e17260016cd4f6c2ee7b6ba5
bhanumurthy1 <31441020+bhanumurthy1@users.noreply.github.com>
2020-02-18 14:13:47 +0530

    Update MyPipeline.groovy

--- vars/MyPipeline.groovy
+++ vars/MyPipeline.groovy
@@ -1,18 +1,7 @@
 def call(Map pipeline)
-{
-    pipeline
-    {
-        agent any 
-        stages
-        {
-            stage("Printing the First parameter")
-            {
-                echo pipeline.branch
-            }
-            stage("Printing the second paarmeter")
-            {
-                echo pipeline.fool
-            }
-        }
-    }
+{    
+    addShortText
+        borderColor : pipeline.bcolor,
+        color : pipeline.ccolor,
+        text : pipeline.texting    
 }
