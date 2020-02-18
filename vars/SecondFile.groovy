d619b658483397bc42997a3fe817d52fe8297775
bhanumurthy1 <31441020+bhanumurthy1@users.noreply.github.com>
2020-02-18 15:43:25 +0530

    Update SecondFile.groovy

--- vars/SecondFile.groovy
+++ vars/SecondFile.groovy
@@ -1,8 +1,5 @@
 def call(Map params)
-{    
-    echo "Hello Job u r going to run now ,${Name}"   
-    build 'Name'
-    echo "Hello Job ur done"    
+{         
     checkout([
     $class: 'GitSCM', 
     branches: [[name: params.branch]],     
