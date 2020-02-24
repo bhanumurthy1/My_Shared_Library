7812be25eae060999558e14c52bd12b5d95a7a63
bhanumurthy1 <31441020+bhanumurthy1@users.noreply.github.com>
2020-02-24 16:50:03 +0530

    Update email.groovy

--- vars/email.groovy
+++ vars/email.groovy
@@ -1,9 +1,13 @@
-def call(String toadd)
+def call(String ...toadd)
 {
+  for(String temp: toadd)
+  {
+    temp1=temp+" ";
+  }
   emailext body: '''Test Mail:<br/>
   Check console output at $BUILD_URL to view the results.<br/>
   <b>Done</b>''', 
   recipientProviders: [culprits()], 
   subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS',
-  to: toadd
+  to: temp1
 }
