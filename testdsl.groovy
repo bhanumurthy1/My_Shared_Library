65102963c8bd44b67876b87748fa21f2071ea597
bhanumurthy1 <31441020+bhanumurthy1@users.noreply.github.com>
2020-02-25 14:51:44 +0530

    Update testdsl.groovy

--- testdsl.groovy
+++ testdsl.groovy
@@ -1,1 +1,29 @@
-println "Hello world"
+pipelineJob('Example')
+{   
+  definition {
+        cpsScm {
+            scm {
+              perforceP4('p4_credentials') {                
+            workspace {
+                manual('jenkins-${NODE_NAME}-${JOB_NAME}-${EXECUTOR_NUMBER}', ' ')             
+            }
+                configure { dum ->dum / populate(class:'org.jenkinsci.plugins.p4.populate.ForceCleanImpl'){
+                                      have(true)
+                                      force(true)            
+                                      quiet(true)           
+                                      
+                                      }
+                dum / workspace / spec / clobber('true')                 
+              	dum / workspace / spec / streamName('//CDG/CDLG/Services/$serviceName/ML/')
+                
+            }
+                  
+                            
+        }           
+          scriptPath('JenkinsFile')
+          lightweight(true)   
+        }
+    }
+  }
+   
+}
