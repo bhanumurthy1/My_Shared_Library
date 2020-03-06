3b9f3569ff68d2dd9cb56b6b290a51c499cd5744
bhanumurthy1 <31441020+bhanumurthy1@users.noreply.github.com>
2020-03-06 11:01:47 +0530

    Update email.groovy

--- vars/email.groovy
+++ vars/email.groovy
@@ -1,9 +1,9 @@
-def call(emailIDs)
+def call(buildStatus,emailIDs)
 {
   // Default values 
   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
   def summary = "${subject} (${env.BUILD_URL})"
-  def details = """<p>$BUILD_STATUS: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
    
   print emailIDs
