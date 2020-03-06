2c54543f20d3a1d1154dfa5e9edc8ff99c0ca0eb
bhanumurthy1 <31441020+bhanumurthy1@users.noreply.github.com>
2020-03-06 11:40:21 +0530

    Update email.groovy

--- vars/email.groovy
+++ vars/email.groovy
@@ -1,3 +1,18 @@
+def call(buildStatus,emailIDs,boolean atchlog)
+{
+  // Default values 
+  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def summary = "${subject} (${env.BUILD_URL})"
+  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+   
+  print emailIDs
+ emailext attachLog: atchlog,   
+  body: details,
+  recipientProviders: [culprits()],
+  subject: subject,
+  to: emailIDs
+}
 def call(buildStatus,emailIDs,boolean atchlog,atchfile)
 {
   // Default values 
@@ -14,3 +29,4 @@ def call(buildStatus,emailIDs,boolean atchlog,atchfile)
   subject: subject,
   to: emailIDs
 }
+
