bbb1e684326ed74a2aadaabb4e02d2d8498c5228
bhanumurthy1 <31441020+bhanumurthy1@users.noreply.github.com>
2020-03-10 16:46:13 +0530

    Create email.groovy

---
+++ email.groovy
@@ -0,0 +1,18 @@
+def call(buildStatus,emailIDs,boolean atchlog,atchfile)
+{
+  // Default values 
+  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def summary = "${subject} (${env.BUILD_URL})"
+  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+   
+  print emailIDs
+ emailext attachLog: atchlog,
+  attachmentsPattern: atchfile,
+  body: details,
+  recipientProviders: [culprits()],
+  subject: subject,
+  to: emailIDs
+}
+
+© 2020 GitHub
