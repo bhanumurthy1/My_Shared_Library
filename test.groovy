0bd120b344bda7030f156963011550ba2133fbab
bhanumurthy1 <31441020+bhanumurthy1@users.noreply.github.com>
2020-03-12 09:41:24 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -15,7 +15,7 @@ def stageTest()
 def stageBuild(String JobName)
 {
        build JobName
-    new email().emailtest("failure","ppravallika@informatica.com",true,"")
+    emailtest("failure","ppravallika@informatica.com",true,"")
 }
 def stageArchive()
 {
@@ -29,5 +29,22 @@ def stagePost()
     echo "failure"
     '''
     
+}
+def emailtest(buildStatus,emailIDs,boolean atchlog,atchfile)
+{
+  // Default values 
+  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def summary = "${subject} (${env.BUILD_URL})"
+  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+   
+  print emailIDs
+ emailext attachLog: atchlog,
+  attachmentsPattern: atchfile,
+  body: details,
+  recipientProviders: [culprits()],
+  subject: subject,
+  to: emailIDs 
+
 }
 return this;
