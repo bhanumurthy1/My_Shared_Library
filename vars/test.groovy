93ba2a3be53108984eb0bbf892062639e1a7eb5d
bhanumurthy1 <31441020+bhanumurthy1@users.noreply.github.com>
2020-03-13 09:38:59 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,14 +1,7 @@
 def call(String vars)
 { 
-  String somevar=vars
-  sh '''
-    email()
-    {
-      echo "hey function"
-      echo $1
-      java $2
-    }
-    email "build compile" "-version"
-  '''
-  
+  String somevar=vars  
+  sh """
+  echo \$vars
+  """  
 }
