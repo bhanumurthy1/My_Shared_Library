cf672fa8c385d2c0c6a8b796bacd694ebf64b2b3
bhanumurthy1 <31441020+bhanumurthy1@users.noreply.github.com>
2020-03-13 12:12:05 +0530

    Update envfile.groovy

--- vars/envfile.groovy
+++ vars/envfile.groovy
@@ -3,9 +3,9 @@ def call(String bCommand)
   script
   {
     echo bCommand
-    export env.buildCommand=bCommand
+    env.buildCommand=bCommand
   }
-  env.buildCommand=bCommand
+ // env.buildCommand=bCommand
   sh '''
   echo $buildCommand
   echo \$bCommand
